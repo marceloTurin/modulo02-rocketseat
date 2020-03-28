b350bd12b64f21d2a62a1140f736de7afbe00253
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2020-03-28 18:33:14 -0300

    Gerando hash de senha

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   // conexao do banco
@@ -7,6 +8,7 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
       },
@@ -14,6 +16,14 @@ class User extends Model {
         sequelize,
       }
     );
+
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
   }
 }
 
