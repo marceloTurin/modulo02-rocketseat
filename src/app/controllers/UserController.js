04e8cee1de8ae5d553947977a456c600821ee618
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2020-03-28 21:12:57 -0300

    Update de usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -19,8 +19,35 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
-    return res.json({ ok: true });
+    const { email, oldPassword } = req.body;
+
+    // Procura o usuário no banco com o ID passado
+    const user = await User.findByPk(req.userId);
+
+    // Verifica se o usuário passou o email e se já não existe o email na base
+    if (email && email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    }
+
+    // Verifica se ele ta passando a senha e se a senha não esta errada
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    // Atualiza os dados e retorna o id,name,provider atualizado
+    const { id, name, provider } = await User.update(req.body);
+
+    // Retorna as informações pro front end
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
